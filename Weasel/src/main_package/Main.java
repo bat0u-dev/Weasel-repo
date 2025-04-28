2887716e739682d13c57df11c9627f9daec128ff
i.roganov <i.roganov@ctrl2go.com>
2025-04-28 22:00:00 +0300

    Multiply minor improvements

--- Weasel/src/main_package/Main.java
+++ Weasel/src/main_package/Main.java
@@ -1,19 +1,20 @@
 package main_package;
 /*
- * -Данная программа должна реализовать пример генезиса нужной нам "сложной" структуры в результате алгоритма имитации
- * искусственного накопительного отбора, при котором каждую отборочную итерацию, в следствие которой промежуточная
+ * -Данная программа должна реализовать пример формирования нужного нам предложения, в результате алгоритма имитации
+ * искусственного накопительного отбора. Каждую отборочную итерацию, вследствие которой промежуточная
  * структура изменяется случайным образом (в рамках заданных условий), программа сохраняет "полезные"
  * свойства промежуточной результирующей структуры. "Полезными" считаются те свойства промежуточной результирующей
- * структуры, в которых она идентична целевой (в данном случае заранее известной) структуре.
+ * структуры, в которых она идентична целевой (в данном случае заранее известной) структуре. Т.е., если совпало место и
+ * значение символа в предложении, то такой символ в нем является его полезнвым свойством.
  * *********************************************************************************************************************
  * Далее возможный вариант реализации программы:
  * 1) Создать массив символов, содержащий латинский алфавит и пробел.
  * 2) Из элементов массива выбранных в случайном порядке формировать предложение содержащее количество символов,
  * равное количеству символов в целевом предложении (?пользователь вводит с клавиатуры).
- * 3) Каждую итерацию формирования "рандомного" предложения посимольно сравниват его с целевым, и сохранять положение
+ * 3) Каждую итерацию формирования "рандомного" предложения посимвольно сравнивать его с целевым, и сохранять положение
  * символа там, где оно совпадает с положением соответствующего символа в целевом предложении (для фиксации "полезных"
- * изменений создается третий массив символов, в последствии из него создается финальная строка).
- * 4) Поворять процедуру отбора и сравнения, пока программа не сформирует предложение полностю идентичное целевому.
+ * изменений создается третий массив символов, впоследствии из него создается финальная строка).
+ * 4) Повторять процедуру отбора и сравнения, пока программа не сформирует предложение полностью идентичное целевому.
  * 5)END.
  */
 
@@ -22,32 +23,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static char[] Symbols = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    private static final char[] Symbols = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};//*hardcode! 27 symbols
-    private static Scanner scr = new Scanner(System.in);
-    private static String FinalSentence;//= "methinks it is like a weasel";
-    private static char[] FSChar;
-    private static char[] IntSChar;
-    private static char[] ResultChar;
+    private static final Scanner scr = new Scanner(System.in);
+    private static String finalSentence;//= "methinks it is like a weasel";
+    private static char[] fSChar;
+    private static char[] intSChar;
+    private static char[] resultChar;
+    private static Random random = new Random();
 
     public static void main(String[] args) throws Exception{
-       FinalSentence = scr.nextLine();
-       FSChar = FinalSentence.toCharArray();
-       ResultChar = new char[FinalSentence.toCharArray().length];
+       finalSentence = scr.nextLine();
+       fSChar = finalSentence.toCharArray();
+       resultChar = new char[finalSentence.length()];
         int counter=0;
         do{
-         IntSChar = createSentence();
-         for(int i=0; i<FSChar.length;i++){
+         intSChar = createSentence();
+         for(int i = 0; i< fSChar.length; i++){
              counter++;
-             if(FSChar[i]==IntSChar[i]){
-                 ResultChar[i]=FSChar[i];
+             if(fSChar[i]== intSChar[i]){
+                 resultChar[i]= fSChar[i];
              }
          }
 
-            System.out.println(IntSChar);
-            System.out.println(charArrToStr(ResultChar) + "\n");
+            System.out.println(intSChar);
+            System.out.println(charArrToStr(resultChar) + "\n");
             Thread.sleep(1200);
-        }while (!(Arrays.toString(FSChar).equals(Arrays.toString(ResultChar))));
+        }while (!(Arrays.toString(fSChar).equals(Arrays.toString(resultChar))));
         System.out.println("Потребовалось " + counter + " шагов.");
     }
 
@@ -61,25 +63,25 @@ public class Main {
             return tempCharArr;
         }
 
-        private static int getRandomElement ( char[] elementArr){
-            int rnd = new Random().nextInt(elementArr.length);
-            return elementArr[rnd];
+        private static int getRandomElement (){
+            int rnd =  random.nextInt(Main.Symbols.length);
+            return Main.Symbols[rnd];
         }
 
 
         private static char[] createSentence () {
             StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < FinalSentence.length(); i++) {
-                builder.append((char) getRandomElement(Symbols));
+            for (int i = 0; i < finalSentence.length(); i++) {
+                builder.append((char) getRandomElement());
             }
             return builder.toString().toCharArray();
         }
 
         private static StringBuilder charArrToStr(char[] inputCharArr){
         StringBuilder builder = new StringBuilder();
-        for(int i=0;i<FinalSentence.length();i++){
-            if(ResultChar[i]=='\u0000')builder.append('-');
-            else{builder.append(ResultChar[i]);}
+        for(int i = 0; i< finalSentence.length(); i++){
+            if(resultChar[i]=='\u0000')builder.append('-');
+            else{builder.append(resultChar[i]);}
         }
         return builder;
         }
